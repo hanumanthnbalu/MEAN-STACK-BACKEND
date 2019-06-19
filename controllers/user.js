de2a0fe07c03df98f303fb5ae0306ac682868f87
hanumanth1 <hanumanth@qwickbit.com>
2019-06-19 16:20:06 +0530

    authj

--- controllers/user.js
+++ controllers/user.js
@@ -52,7 +52,7 @@ exports.createUser = async (req, res, next) => {
 						</tr> 
 						<tr height="50"> 
 						<td align="left" valign="middle"> 
-							<img src="https://lh3.googleusercontent.com/-VTs6GEhLon4/WMA7Fo5sSwI/AAAAAAAAEtY/dAZAoAGwjSMS79ITFsJiG3Mqb5wcmSYewCEwYBhgL/w140-h139-p/IMG_6983.JPG" width="50" height="50" style="border:0;width:51px;height:50px" class="CToWUd">
+							<img src="https://lh3.googleusercontent.com/-VTs6GEhLon4/WMA7Fo5sSwI/AAAAAAAAEtY/dAZAoAGwjSMS79ITFsJiG3Mqb5wcmSYewCEwYBhgL/w140-h139-p/IMG_6983.JPG" width="50" height="50" style="border:0;width:51px;height:50px;border-radius: 50px;" class="CToWUd">
 						</td> 
 						<td align="right" valign="middle"> <span style="text-align:right!important;font-size:12px;color:#ffffff;font-weight:600">HANUMANTHNBALU</span> </td> 
 						</tr> 
@@ -77,7 +77,13 @@ exports.createUser = async (req, res, next) => {
 						<table class="m_-4611902048954939277m_7484439867955395762inner-table" summary="inner-table" align="center" valign="top" border="0" cellpadding="0" cellspacing="0" style="width:96%;max-width:490px;margin:0 auto;background-color:#ffffff;background-image:url(https://ci6.googleusercontent.com/proxy/iX49tT0UKKLPmVXIH8NYw_2yjJkLsObn1J4PwRYP5e3eDm5r91DEH_nAZVQZjFRYn6PQhyrFrJujMoEi7aQoJIwb4b05Z-ROZgk6tWCQWTHzAr_NSw=s0-d-e1-ft#http://pages.getpostman.com/rs/067-UMD-991/images/image-shadow.png);background-repeat:repeat-x;background-size:2px 25px;background-position:0px 0px;margin:0 auto;text-align:center" width="100%" bgcolor="#FFFFFF"> 
 						<tbody> 
 						<tr> 
-						<td> <h1 style="font-family:'Open Sans',-apple-system,Helvetica,Arial,sans-serif!important;font-weight:600;margin:25px 20px 25px 20px;padding:0px;text-align:center;color:#282828;font-size:20px">HANUMANTHNBALU</h1> 
+						<td> 
+						<h1 style="font-family:'Open Sans',-apple-system,Helvetica,Arial,sans-serif!important;font-weight:600;margin:25px 20px 25px 20px;padding:0px;text-align:center;color:#282828;font-size:20px">HANUMANTHNBALU</h1> 
+						</td> 
+						</tr> 
+						<tr> 
+						<td> 
+						<h3 style="text-align: center;">Thank you!.</h3>
 						</td> 
 						</tr> 
 						</tbody> 
@@ -108,17 +114,6 @@ exports.createUser = async (req, res, next) => {
 						</tbody> 
 						</table>
 				`
-				// html: `<div style="background: #da1d22;text-align:center;padding:20px;border-radius:10px;width:70%;border:1px solid #cdcdcd;margin:0 auto;color: #110e0e;">	<div style="width: 100%;margin: 0 auto;display: block;"> 
-				// <img style="box-shadow: 0 8px 29px 2px rgba(66, 66, 66, 0.8);border-radius: 100px;" src="https://lh3.googleusercontent.com/-VTs6GEhLon4/WMA7Fo5sSwI/AAAAAAAAEtY/dAZAoAGwjSMS79ITFsJiG3Mqb5wcmSYewCEwYBhgL/w140-h139-p/IMG_6983.JPG" alt="Logo">
-				// </div>
-				// <strong style="color:#66cb86;padding: 10px 0px;font-weight: 900;">Hi, ${user.username}</strong>
-				// <h1>Welcome to HNBALU</h1>
-				// <p>User Details:</p>
-				// <p>Username: ${user.username}</p>
-				// <p>E-mail: ${user.email}</p>
-				// <p>Password: ${user.password}</p>
-				// </div>
-				// 	  `
 			});
 			res.status(201).send({
 				message: 'user created successfuly!!',
